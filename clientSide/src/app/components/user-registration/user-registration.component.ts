39c45b9c918391c7b9d0d7dde2540877b967595f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-07 16:49:00 -0300

    movendo a pasta components para a pasta app.

--- clientSide/src/app/components/user-registration/user-registration.component.ts
+++ clientSide/src/app/components/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user-registration.component.scss'
 })
 export class UserRegistrationComponent {
-
+  constructor(private formBuilder: FormBuilder){}   
 }
