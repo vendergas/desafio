4c125a42a9e39bc5f3300114a37044a92a11b2ad
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-07 11:03:04 -0300

    criacao do componente user-registration. este componente e responsavel pela tela de cadastro do usuario.

---
+++ clientSide/src/components/user-registration/user-registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-registration.component.html',
+  styleUrl: './user-registration.component.scss'
+})
+export class UserRegistrationComponent {
+
+}
