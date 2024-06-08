5f5138077dde141c41312f19d3b8af835651c108
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-08 18:09:25 -0300

    inicio do desenvolvimento do componente responsavel pela autenticacao do usuario.

--- clientSide/src/app/components/user-authentication/user-authentication.component.ts
+++ clientSide/src/app/components/user-authentication/user-authentication.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserAuthenticationService } from '../services/user-authentication/user-authentication.service';
 
 @Component({
   selector: 'app-user-authentication',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './user-authentication.component.scss'
 })
 export class UserAuthenticationComponent {
+  authenticationUser: FormGroup;
+  success: boolean = false;
+  fail: boolean = false;
 
+  constructor(private formBuilder: FormBuilder, private userAuthenticationService: UserAuthenticationService){
+    this.authenticationUser = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", [Validators.min(8), Validators.required]]
+    });
+  }
+
+  get formControls(){
+    return this.authenticationUser.controls;
+  }
+
+  onSubmit(){
+    this.userAuthenticationService
+  }
 }
