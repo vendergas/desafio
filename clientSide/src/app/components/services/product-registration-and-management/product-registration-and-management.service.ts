9fd17a98d4bef4890c77fba8c19205ab029441ac
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 12:37:01 -0300

    adicao das funcoes para editar e deletar os dados dos produtos.

--- clientSide/src/app/components/services/product-registration-and-management/product-registration-and-management.service.ts
+++ clientSide/src/app/components/services/product-registration-and-management/product-registration-and-management.service.ts
@@ -1,21 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductRegistrationAndManagementService {
-  private urlApi = "";
+  private urlApi = "http://localhost:3000/products";
   
   constructor(private http: HttpClient) { }
 
-  register(product: FormGroup): Observable<FormGroup>{
-    return this.http.post<FormGroup>(this.urlApi, product)
+  register(product: any): Observable<any>{
+    return this.http.post<any>(this.urlApi, product);
   };
 
-  getAll(): Observable<FormGroup>{
-    return this.http.get<FormGroup>(this.urlApi)
+  getAll(): Observable<any>{
+    return this.http.get<any>(this.urlApi);
   };
+
+  edit(product: any): Observable<any>{
+    return this.http.put<any>(`${this.urlApi}/${product.id}`, product);
+  }
+
+  deleteProduct(id: string): Observable<any>{
+    return this.http.delete(`${this.urlApi}/${id}`);
+  }
 }
