649135f753b5c58f42a9a4b2bd4b226f2cd87c34
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 10:08:01 -0300

    criacao da logica do componente productRegistrationAndManagement.

--- clientSide/src/app/components/product-registration-and-management/product-registration-and-management.component.ts
+++ clientSide/src/app/components/product-registration-and-management/product-registration-and-management.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductRegistrationAndManagementService } from '../services/product-registration-and-management/product-registration-and-management.service';
 
 @Component({
   selector: 'app-product-registration-and-management',
@@ -8,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrl: './product-registration-and-management.component.scss'
 })
 export class ProductRegistrationAndManagementComponent {
+  product!: FormGroup;
+  products!: FormGroup;
+  submitted: boolean = false;
+  success: boolean = false;
+  fail: boolean = false;
 
+  constructor(private formBuilder: FormBuilder, private productRegistrationAndManagementService: ProductRegistrationAndManagementService){}
+
+  ngOnInit(){
+    this.product = this.formBuilder.group({
+      name: ["", Validators.required],
+      value: ["", Validators.required],
+      description: ["", Validators.required],
+      company: ["", Validators.required]
+    });
+    this.showProducts();
+  }
+
+  showProducts(){
+    this.productRegistrationAndManagementService.getAll().subscribe((data) => this.products = data);
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    this.productRegistrationAndManagementService.register(this.product).subscribe({
+      next: response => {
+        console.log(`response: ${response}`);
+        this.success = true;
+      },
+
+      error: error => {
+        console.log(`error: ${error}`);
+        this.fail = true;
+      }
+    });
+  }
 }
