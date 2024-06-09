8b6686bc5516d8deaa5f1f4dd395d377c0a14479
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 17:21:11 -0300

    refatoracao do codigo. remocao do service order-release e alteracao do order-management para ser apenas order e ser usado nas duas telas.

--- clientSide/src/app/components/services/order/order.service.ts
+++ clientSide/src/app/components/services/order/order.service.ts
@@ -5,12 +5,16 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class OrderManagementService {
+export class OrderService {
   private urlApi = "http://localhost:3000/api/orders"
   constructor(private http: HttpClient){}
 
   getAll(): Observable<any>{
     return this.http.get<any>(this.urlApi);
+  }
+
+  generateOrder(order: any): Observable<any>{
+    return this.http.post<any>(this.urlApi, order);
   }
 
   deleteOrder(id: string): Observable<any>{
