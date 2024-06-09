c6ef794fd8db41fadfe46615cfcadbb6e2786126
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 15:06:50 -0300

    desenvolvimento da logica do service orderManagement.

--- clientSide/src/app/components/services/order-management/order-management.service.ts
+++ clientSide/src/app/components/services/order-management/order-management.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderManagementService {
+  private urlApi = "http://localhost:3000/api/orders"
+  constructor(private http: HttpClient){}
 
-  constructor() { }
+  getAll(): Observable<any>{
+    return this.http.get<any>(this.urlApi);
+  }
+
+  deleteOrder(id: string): Observable<any>{
+    return this.http.delete<any>(`${this.urlApi}/${id}`);
+  }
 }
