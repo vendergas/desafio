6a15c3ac3fa5631869ae7a6d68dac0f8378f81e3
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 10:11:42 -0300

    programacao do service productRegistrationAndManagement.

--- clientSide/src/app/components/services/product-registration-and-management/product-registration-and-management.service.ts
+++ clientSide/src/app/components/services/product-registration-and-management/product-registration-and-management.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductRegistrationAndManagementService {
+  private urlApi = "";
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  register(product: FormGroup): Observable<FormGroup>{
+    return this.http.post<FormGroup>(this.urlApi, product)
+  };
+
+  getAll(): Observable<FormGroup>{
+    return this.http.get<FormGroup>(this.urlApi)
+  };
 }
