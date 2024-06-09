28b7139db2e4ff0a1039bebe0b01a9f6f4b7ce13
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 20:36:53 -0300

    desenvolvimento da logica do componente orderRelease.

--- clientSide/src/app/components/order-release/order-release.component.ts
+++ clientSide/src/app/components/order-release/order-release.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OrderService } from '../services/order/order.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order-release',
@@ -8,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './order-release.component.scss'
 })
 export class OrderReleaseComponent {
+  order: FormGroup;
+  submitted: boolean = false;
+  success: boolean = false;
+  fail: boolean = false;
 
+  constructor(private formBuilder: FormBuilder, private orderService: OrderService){
+    this.order = this.formBuilder.group({
+      orderNumber: ["", Validators.required],
+      orderClientName: ["", Validators.required],
+      orderCompanyName: ["", Validators.required],
+      orderDetails: ["", Validators.required],
+      orderDate: ["", Validators.required]
+    })
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    this.orderService.generateOrder(this.order).subscribe({
+      next: request => {
+        console.log(`Request: ${request}`);
+        this.success = true;
+      },
+      error: error => {
+        console.log(`Error: ${error}`);
+        this.fail = true;
+      }
+    })
+  }
 }
