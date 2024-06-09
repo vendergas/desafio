989c278b6b705c15905c24b53ee1266869afd94b
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-09 13:46:51 -0300

    desenvolvimento da logica do componente companyRegistrationAndManagement.

--- clientSide/src/app/components/company-registration-and-management/company-registration-and-management.component.ts
+++ clientSide/src/app/components/company-registration-and-management/company-registration-and-management.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CompanyRegistrationAndManagementService } from '../services/company-registration-and-management/company-registration-and-management.service';
 
 @Component({
   selector: 'app-company-registration-and-management',
@@ -8,5 +10,62 @@ import { Component } from '@angular/core';
   styleUrl: './company-registration-and-management.component.scss'
 })
 export class CompanyRegistrationAndManagementComponent {
+  company: FormGroup;
+  companies = [];
+  submitted: boolean = false;
+  success: boolean = false;
+  fail: boolean = false;
 
+  constructor(private formBuilder: FormBuilder, private companyRegistrationAndManagementService: CompanyRegistrationAndManagementService){
+    this.company = this.formBuilder.group({
+      id: [null],
+      fantasyName: ["", Validators.required],
+      corporateReason: ["", Validators.required],
+      cnpj: ["", Validators.required]
+    });
+  }
+
+  ngOnInit(){
+    this.showcompanys();
+  }
+
+  get formControls(){
+    return this.company.controls;
+  }
+
+  showcompanys(){
+    this.companyRegistrationAndManagementService.getAll().subscribe((data) => this.companies = data);
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.company.value.id){
+      this.companyRegistrationAndManagementService.edit(this.company.value).subscribe(() => {
+        this.showcompanys();
+        this.resetForm();
+      });
+    } else{
+      this.companyRegistrationAndManagementService.register(this.company).subscribe(() => {
+        this.showcompanys();
+        this.resetForm();
+      });
+    }
+  }
+
+  onEdit(companyForm: FormGroup){
+    this.company.setValue(companyForm);
+  }
+
+  edit(id: string){
+    this.companyRegistrationAndManagementService.edit(id);
+  }
+
+  delet(id: string){
+    this.companyRegistrationAndManagementService.deleteCompany(id);
+  }
+
+  resetForm(){
+    this.company.reset({id: null, fantasyName: "", corporateReason: "", cnpj: ""});
+  }
 }
