23083354ec0a6429ababf6f235a17cf87f0ee6e3
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-10 14:06:10 -0300

    correcao nos metodos get e post, que estavam em chamadas erradas.

--- serverSide/src/routes/company.routes.js
+++ serverSide/src/routes/company.routes.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", companyController.create);
-router.post("/", companyController.findAll);
+router.post("/", companyController.create);
+router.get("/", companyController.findAll);
 router.put("/:id", companyController.update);
 router.delete("/:id", companyController.delete);
 
