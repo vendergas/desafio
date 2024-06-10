f1c76b416453adb33c1fd5bee9b670d351b64ac4
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-10 11:09:04 -0300

    desenvolvimento da configuracao dos models.

---
+++ serverSide/src/models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const dbConfig = require("../config/db.config");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user.model.js")(sequelize, Sequelize);
+db.company = require("./company.model.js")(sequelize, Sequelize);
+db.customer = require("./customer.model.js")(sequelize, Sequelize);
+db.product = require("./product.model.js")(sequelize, Sequelize);
+db.order = require("./order.model.js")(sequelize, Sequelize);
+db.orderProduct = require("./orderProduct.model.js")(sequelize, Sequelize);
